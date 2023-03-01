b579de779a86f9f67eb954f8ec8331dc0e9938c9
Sebastián Valdivia <srvaldivia@gmail.com>
2023-03-01 14:33:35 -0300

    Ordenar script inicial

--- ordenar_datos_deis.R
+++ ordenar_datos_deis.R
@@ -1,49 +1,15 @@
 
 # packages ----------------------------------------------------------------
 
-
-pacman::p_load(tidyverse, arrow, install = FALSE)
-
-
-library(tidyverse)
-# library(sf)
-library(arrow)
+pacman::p_load(tidyverse, arrow, here, install = FALSE)
 
 
-
-# parquet format ----------------------------------------------------------
-
-## 2019
-# readxl::read_excel(path = "01_Paper mane/datos_input/AtencionesUrgencia2019.xlsx") |>
-#   write_parquet(sink = "01_Paper mane/datos_input/au_2019.parquet")
-
-
-# microbenchmark::microbenchmark(
-#   rbase = readxl::read_excel(path = "01_Paper mane/datos_input/AtencionesUrgencia2019.xlsx"),
-#   parquet = read_parquet("01_Paper mane/datos_input/au_2019.parquet"),
-#   times = 3)
-
-
-## 2018
-# readxl::read_excel(path = "01_Paper mane/datos_input/AtencionesUrgencia2018.xlsx") |>
-#   write_parquet(sink = "01_Paper mane/datos_input/au_2018.parquet")
-
-
-# microbenchmark::microbenchmark(
-#   rbase = readxl::read_excel(path = "01_Paper mane/datos_input/AtencionesUrgencia2018.xlsx"),
-#   parquet = read_parquet("01_Paper mane/datos_input/au_2018.parquet"),
-#   times = 2)
 
 # read data ---------------------------------------------------------------
 
-urgencias_2019 <- read_parquet("01_Paper mane/datos_input/au_2019.parquet")
+urgencias_2019 <- read_parquet(here("datos_input", "au_2019.parquet"))
 
-
-urgencias_2018 <- read_parquet("01_Paper mane/datos_input/au_2018.parquet")
-
-# md_2019 <- readxl::read_excel(path = "01_Paper mane/datos_input/ATENCIONES_DE_URGENCIA_2019.xlsx",
-#                               sheet = "Hoja1")
-
+urgencias_2018 <- read_parquet(here("datos_input", "au_2018.parquet"))
 
 
 
@@ -145,13 +111,6 @@ urgencias_2018 <- urgencias_2018 |>
                            "Otra causa respiratoria (J22, J30-J39, J47, J60-J98)"))
 
 ## 2019 (17) tiene más establecimientos que 2018 (13)
-# a <- urgencias_2018 |>
-#   count(idestablecimiento, nestablecimiento) |> arrange(idestablecimiento)
-# 
-# b <- urgencias_2019 |>
-#   count(idestablecimiento, nestablecimiento) |> arrange(idestablecimiento)
-# 
-# full_join(x = a, y = b, by = "idestablecimiento", keep = TRUE) |> view()
 
 
 
@@ -193,14 +152,7 @@ serie_tiempo_2018 <- base_2018 |>
 # plots -------------------------------------------------------------------
 
 
-ggplot(serie_tiempo_2019 |> filter(glosacausa == "TOTAL CAUSAS SISTEMA CIRCULATORIO")) +
-  geom_point(aes(x = n_semana, y = n_15_64, colour = glosacausa)) +
-  geom_line(aes(x = n_semana, y = n_15_64, colour = glosacausa)) +
-  # scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-  theme(legend.position = "bottom")
-
-
-  
+# 2019
 ggplot(serie_tiempo_2019) +
   geom_point(aes(x = n_semana,
                  y = n_15_64,
@@ -213,13 +165,7 @@ ggplot(serie_tiempo_2019) +
 
 
 
-ggplot(serie_tiempo_2018 |> filter(glosacausa == "TOTAL CAUSAS SISTEMA CIRCULATORIO")) +
-  geom_point(aes(x = semana, y = n_15_64, colour = glosacausa)) +
-  geom_line(aes(x = semana, y = n_15_64, colour = glosacausa)) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-  theme(legend.position = "bottom")
-
-
+# 2018
 ggplot(serie_tiempo_2018) +
   geom_point(aes(x = n_semana,
                  y = n_15_64,
@@ -229,26 +175,3 @@ ggplot(serie_tiempo_2018) +
                 colour = glosacausa)) +
   facet_wrap(~ glosacausa) +
   theme(legend.position = "bottom")
-
-
-
-
-
-# test azure --------------------------------------------------------------
-library(Microsoft365R)
-
-odb <- get_business_onedrive()
-
-
-
-odb$list_items()
-
-list_sharepoint_sites()
-
-
-
-
-
-
-site <- get_sharepoint_site("uc365_datamet")
-
